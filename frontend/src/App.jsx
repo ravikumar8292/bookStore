b829112995b596aebbf32f3c76df7e3ec02f7f98
ravikumar8292 <ravi.kumarrfj@gmail.com>
2024-05-20 00:43:50 +0530

    Navbar complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <div className="">
+      <Navbar />
+      <Banner />
+      <Footer />
+    </div>
+    </>
+  )
+}
+
+export default App
